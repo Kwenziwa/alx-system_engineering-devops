b0d8453d7871607072bbe018a64de604cbec918f
kwenziwa <kwenziwa@live.com>
2023-10-10 15:48:55 +0000

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response_data = requests.get(api_url, headers=headers, allow_redirects=False)
+    if response_data.status_code == 404:
+        return 0
+    rresults_data = response_data.json().get("data")
+    return rresults_data.get("subscribers")
